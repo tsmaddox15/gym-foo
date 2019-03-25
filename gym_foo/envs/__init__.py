b26f4571673e2030c0d1223ec7794c74d72a62b4
Taylor <tsmaddox15@gmail.com>
2019-03-25 18:49:33 -0400

    first commit

---
+++ gym_foo/envs/__init__.py
@@ -0,0 +1,7 @@
+from gym_foo.envs.foo_env import FooEnv
+# from gym.envs.registration import register
+#
+# register(
+#     id='foo-v0',
+#     entry_point='myenv.myenv:MyEnv',
+# )
---
+++ test.py
@@ -0,0 +1,4 @@
+import gym
+import gym_foo
+
+env = gym.make('gym_foo:foo-v0')
