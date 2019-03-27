52b768c8efba92ff8b2555ab0181e7e32ceb7c53
tsmaddox15 <tsmaddox15@gmail.com>
2019-03-27 14:40:50 -0400

    change example to lunar lander

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
 import gym
 import gym_foo
 
+# import gym.envs.box2d.lunar_lander
+
+# import gym
 env = gym.make('foo-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+env.reset()
+env.render()
